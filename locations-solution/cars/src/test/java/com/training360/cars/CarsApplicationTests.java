7e0f1980fabe5b1aa84e2f0f769e61c8299730a2
Lipek <lipek71@gmail.com>
2021-06-23 10:30:51 +0200

    cars

---
+++ locations-solution/cars/src/test/java/com/training360/cars/CarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training360.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
