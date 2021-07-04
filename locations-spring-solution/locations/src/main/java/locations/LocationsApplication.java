ad5b3ac510d44fbcff7a35691ca03f8bf882ff90
Lipek <lipek71@gmail.com>
2021-07-04 23:44:24 +0200

    Státuszkódok és hibakezelés. Státuszkódok beállítva, Problem használatával RFC szabvány JSON generálása, saját kivételkezelés beállítva.

--- locations-spring-solution/locations/src/main/java/locations/LocationsApplication.java
+++ locations-spring-solution/locations/src/main/java/locations/LocationsApplication.java
@@ -1,5 +1,6 @@
 package locations;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,11 @@ public class LocationsApplication {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper().findAndRegisterModules();
     }
 //    @Bean
 //    public LocationService locationService(){
--- locations-spring-solution/locations/src/main/java/locations/LocationsController.java
+++ locations-spring-solution/locations/src/main/java/locations/LocationsController.java
@@ -1,8 +1,13 @@
 package locations;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -53,10 +58,26 @@ public class LocationsController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteFavouritePlace(@PathVariable("id") long id) {
         locationService.deleteFavouritePlace(id);
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Problem> LocationNotFoundException(IllegalArgumentException iae) {
+        Problem problem =
+                Problem.builder()
+                        .withType(URI.create("location/not-found"))
+                        .withTitle("Not found")
+                        .withStatus(Status.NOT_FOUND)
+                        .withDetail(iae.getMessage())
+                        .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+                .body(problem);
+    }
 
 
 }
