23acdbb6942fb745174df693fea00ba45ae8cece
Lipek <lipek71@gmail.com>
2021-07-07 18:52:28 +0200

    Cars feladat majdnem kész állapotban

--- cars/src/main/java/com/training360/cars/CarsApplication.java
+++ cars/src/main/java/com/training360/cars/CarsApplication.java
@@ -1,13 +1,26 @@
 package com.training360.cars;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CarsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CarsApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper().findAndRegisterModules();
     }
 
 }
