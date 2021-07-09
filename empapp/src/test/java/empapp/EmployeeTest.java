709a3433d35abd651763afc9ef8f0323befb7819
Lipek <lipek71@gmail.com>
2021-07-10 01:20:55 +0200

    Tesztesetek életciklusa

--- empapp/src/test/java/empapp/EmployeeTest.java
+++ empapp/src/test/java/empapp/EmployeeTest.java
@@ -1,12 +1,34 @@
 package empapp;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class EmployeeTest {
 
+    Employee employee;
+
+    public EmployeeTest(){
+        System.out.println("CONSTRUCTOR");
+    }
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("BEFORE ALL");
+    }
+
+    @BeforeEach
+    void initEmployee(){
+        employee = new Employee("John Doe", 1970);
+        System.out.println("INIT");
+    }
+
     @Test
+    @Order(1)
+    @DisabledOnOs(OS.WINDOWS)
     void testGetAge(){
         //Given
         //Employee employee = new Employee("John Doe", 1970);
@@ -15,7 +37,16 @@ class EmployeeTest {
         //Then
         //assertEquals(30, age);
 
-        assertEquals(30, new Employee("John Doe", 1970).getAge(2000));
+        assertEquals(30, employee.getAge(2000));
+        System.out.println("TC1");
+    }
+
+    @Test
+    @Order(2)
+    @Disabled("Until fix 123")
+    void testGetAgeWithZero(){
+        assertEquals(0,  employee.getAge(1970));
+        System.out.println("TC2");
     }
 
 }
